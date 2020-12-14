d83037db20565ea62697ecebd48323c5d7d0b2a6
Jaising <987323161@qq.com>
2020-12-14 15:44:51 +0800

    Initial commit

---
+++ src/main/java/org/star/easyws/EasyWebSocketApplication.java
@@ -0,0 +1,13 @@
+package org.star.easyws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyWebSocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyWebSocketApplication.class, args);
+	}
+
+}
