83df03f1085711210c26086665106e82b1e544cc
Jaising <987323161@qq.com>
2020-12-15 17:28:24 +0800

    feat: Directory refactor and Rename

--- easyws-spring-boot-autoconfigure/src/main/java/org/star/easyws/broker/WebSocketInboundInterceptor.java
+++ easyws-spring-boot-autoconfigure/src/main/java/org/star/easyws/broker/WebSocketInboundInterceptor.java
@@ -1,5 +1,6 @@
 package org.star.easyws.broker;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.support.ChannelInterceptor;
@@ -10,11 +11,14 @@ import org.springframework.stereotype.Component;
  * @description WebSocket Inbound Interceptor
  * @date 2020/12/14
  */
+@Slf4j
 @Component
 public class WebSocketInboundInterceptor implements ChannelInterceptor {
 
     @Override
     public Message<?> preSend(Message<?> message, MessageChannel channel) {
+        String simpSessionId = String.valueOf(message.getHeaders().get("simpSessionId"));
+        log.info("Receive a client connect, simpSessionId:{}", simpSessionId);
         return null;
     }
 }
